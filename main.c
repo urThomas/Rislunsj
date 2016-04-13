8e82b7a333fcd7ac04b8ef45b705e5e42c989b2c
urThomas <pi@raspberrypi.(none)>
2016-04-13 12:36:09 +0000

    PWM upn running

--- main.c
+++ main.c
@@ -5,16 +5,10 @@
 #include<time.h>
 #include<math.h>
 #include"ADC_driver.h"
+//#include<wiringPi.h>
+#include<softPwm.h>
 //#include"track_simulation.h"
 //#include"io.h"
-
-
-// HEIHEIHEIHEIEHIEH
-// Heuheu
-
-
-//bajs
-
 
 
 int main(int argc, char** arg){
@@ -22,11 +16,29 @@ int main(int argc, char** arg){
         int torque = calculate_torque(3.3, 5);
         printf("torque = %d, floatnumber = %f \n", torque, 5.5);
         */
-
+	
+	/*	
+	
 	ADC_init();
 	while(1){
 		printf("reading from ADC: %i \n", ADC_read());
 	}
+	*/
+	wiringPiSetupGpio();
+	int pwm_error = softPwmCreate(24, 10, 100);
+	if (pwm_error != 0 ){
+		printf("PWM-setup error\n");
+	}
+	
+	while (1){
+		for(int i = 0; i < 100; i++){
+			softPwmWrite(24, i );
+		}
+	}
+	
+		softPwmWrite(24, 50 );
+	while(1){}
+
         return 0;
 }
 
